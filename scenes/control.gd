95862ffa9bb9c0e8bb0e82dfb43207c01fefaf88
Rafael Cacau <rafael.cacau@p4x.com.br>
2026-09-16 20:07:32 -0300

    a

--- scenes/control.gd
+++ scenes/control.gd
@@ -2,11 +2,30 @@ extends Control
 
 var elapsed_time := 0.0
 
-var max_health := 100
-var health := 100
+# Cada ponto de vida representa meio coração.
+const MAX_HEALTH := 6
+const HEART_FULL = preload("res://sprites/kenney_pixel-platformer/Tiles/tile_0044.png")
+const HEART_HALF = preload("res://sprites/kenney_pixel-platformer/Tiles/tile_0045.png")
+const HEART_EMPTY = preload("res://sprites/kenney_pixel-platformer/Tiles/tile_0046.png")
+
+var health := MAX_HEALTH
 
 @onready var timer_label = $TimerLabel
-@onready var health_label = $HealthLabel
+@onready var hearts = $Hearts.get_children()
+
+func _ready() -> void:
+	atualizar_coracoes()
+
+func atualizar_coracoes() -> void:
+	for index in range(hearts.size()):
+		var heart_health = clampi(health - index * 2, 0, 2)
+		match heart_health:
+			2:
+				hearts[index].texture = HEART_FULL
+			1:
+				hearts[index].texture = HEART_HALF
+			0:
+				hearts[index].texture = HEART_EMPTY
 
 func _process(delta):
 	elapsed_time += delta
@@ -16,19 +35,16 @@ func _process(delta):
 
 	timer_label.text = "%02d:%02d" % [minutes, seconds]
 
-	health_label.text = "Vida: %d" % health
-
-
 func tirar_vida():
-	health -= 10
-	health = max(0, health)
+	health = maxi(0, health - 1)
+	atualizar_coracoes()
 
 func _on_damage_button_pressed() -> void:
 	tirar_vida()
 
 func _on_heal_button_pressed() -> void:
-	health += 10
-	health = min(100, health)
+	health = mini(MAX_HEALTH, health + 1)
+	atualizar_coracoes()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == get_node("../../Player"):
